af64261d82b9e3cbcb0ca138d736d17fecee3757
Heather Kim <haesoo1108@gmail.com>
2020-05-03 12:45:29 +0900

    :sparkles: Added interactableObject class

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    public string interactionText;
+
+    public bool showOptions;
+
+    public string[] options;
+    public bool[] endingTriggers;
+}
