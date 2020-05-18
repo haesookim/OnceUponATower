3b0a6e6f3110d30f4c73f966664ca279a2a58b3d
Heather Kim <haesoo1108@gmail.com>
2020-05-18 20:48:57 +0900

    Added escape book interaction

---
+++ Assets/Scripts/NPCs/escapeBookInteraction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class escapeBookInteraction : NPCInteraction
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        NPCName="탈출의 상식";
+        infoA = "탈출의 상식이 가득 적혀있는 책이다.";
+
+        hasOptions = true;
+        options = new string[]{"읽는다."};
+    }
+
+    public override string selectOption(int optionNo){
+        if (optionNo == 0){
+            //show morse code image;
+        }
+        return null;
+    }
+}
