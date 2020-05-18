2052d35ecead2218660541d9f99bb89b4d2222c6
sohhyung <lgb0324@naver.com>
2020-05-18 19:34:14 +0900

    apple, npc tag added

---
+++ Assets/Scripts/Objects/appleinteraction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class appleinteraction : InteractableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemName = "사과";
+        infoA = "탐스러운 사과다. 조금 수상하다.";
+        infoB = "챙긴다";
+
+        hasOptions = true;
+        options = new string[]{"먹는다"};
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
