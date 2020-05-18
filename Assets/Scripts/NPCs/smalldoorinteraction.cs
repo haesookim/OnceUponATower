5b5136852efb539226d2cc823113b0d8ef7484e1
sohhyung <lgb0324@naver.com>
2020-05-18 22:55:58 +0900

    small door

--- Assets/Scripts/NPCs/smalldoorinteraction.cs
+++ Assets/Scripts/NPCs/smalldoorinteraction.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class smalldoorinteraction : MonoBehaviour
+public class smalldoorinteraction : NPCInteraction
 {
+  public GameObject PlayerObject;
+  private PlayerInteraction Player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        NPCName = "작은 문";
+        infoA = "작은 문이다.";
+
+        hasOptions = true;
+        actionText = new List<string>{"너무 작아 들어갈 수 없다."};
+
+        Player = PlayerObject.GetComponent<PlayerInteraction>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public override string selectOption(int optionNo){
+        if (optionNo == 0){
+          if(Player.actionConditions[6]){
+            //animation 엄지공주가 문으로 들어가는
+
+            Player.TriggerEnding(17);
+
+          }
+
+        }
+        return null;
     }
+
+
 }
