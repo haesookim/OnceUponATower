ef825972c5e798e0a32d0c5b636b7270f2bc236c
sohhyung <lgb0324@naver.com>
2020-05-19 04:07:02 +0900

    triggerending edited

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerInteraction : MonoBehaviour
-{   
+{
     // Canvas Text interaction
     public Canvas dialogueCanvas;
     public GameObject optionItem;
     public GameObject optionsParent;
-    public GameObject optionsBox;
-    public GameObject optionSelector;
 
     private bool dialogueActive;
     private bool NPCActive;
@@ -22,10 +20,7 @@ public class PlayerInteraction : MonoBehaviour
     public Canvas doorCanvas;
     public GameObject doorParent;
     private bool doorActive;
-    
-    public GameObject doorBox;
-    public GameObject doorSelector;
-        public bool teleported = false;
+    public bool teleported = false;
 
     private Door currentDoor;
 
@@ -38,7 +33,7 @@ public class PlayerInteraction : MonoBehaviour
 
     //conditions checker;
     public Dictionary<int, bool> actionConditions = new Dictionary<int, bool>{
-        {1, false}, 
+        {1, false},
         {2, false},
         {3, false},
         {6, false}
@@ -48,8 +43,8 @@ public class PlayerInteraction : MonoBehaviour
         dialogueCanvas = GameObject.Find("interactionCanvas").GetComponent<Canvas>();
         inventoryCanvas = GameObject.Find("inventoryCanvas").GetComponent<Canvas>();
         doorCanvas = GameObject.Find("DoorCanvas").GetComponent<Canvas>();
-        
-        
+
+
         inventory = gameObject.GetComponent<PlayerInventory>();
 
     }
@@ -64,7 +59,7 @@ public class PlayerInteraction : MonoBehaviour
 
             if (col.tag == "interactableObject"){
                 NPCActive = false;
-                currentObj = col.gameObject.GetComponent<InteractableObject>(); 
+                currentObj = col.gameObject.GetComponent<InteractableObject>();
                 GameObject.Find("ObjName").GetComponent<Text>().text = currentObj.itemName;
                 GameObject.Find("infoA").GetComponent<Text>().text = currentObj.infoA;
                 GameObject.Find("infoB").GetComponent<Text>().text = currentObj.infoB;
@@ -90,13 +85,13 @@ public class PlayerInteraction : MonoBehaviour
                         newOption.transform.SetParent(optionsParent.transform, false);
                     }
                 }
-            } 
+            }
         } else if (col.tag == "NPC_portrait"){
             dialogueActive = true;
             NPCActive = true;
             currentNPC = col.gameObject.GetComponent<NPCInteraction>();
-            //GameObject.Find("ObjName").GetComponent<Text>().text = currentNPC.NPCName;
-        } 
+            GameObject.Find("ObjName").GetComponent<Text>().text = currentNPC.NPCName;
+        }
          else if(col.tag == "dragon"){
             dialogueActive = true;
             NPCActive = true;
@@ -116,7 +111,6 @@ public class PlayerInteraction : MonoBehaviour
 
             if (!actionConditions[2]){
                 gameObject.GetComponent<PrincessMove>().enabled = false;
-
             }
         }
 
@@ -127,7 +121,7 @@ public class PlayerInteraction : MonoBehaviour
             currentDoor = col.gameObject.GetComponent<Door>();
 
             GameObject.Find("DoorName").GetComponent<Text>().text = currentDoor.positionName;
-            
+
             for (int i = 0; i < currentDoor.goalPosition.Length ; i++){
                 GameObject newOption = Instantiate(optionItem, doorParent.transform);
                 newOption.transform.GetChild(0).GetComponent<Text>().text = currentDoor.goalPosition[i].positionName;
@@ -158,27 +152,79 @@ public class PlayerInteraction : MonoBehaviour
     }
 
     public void TriggerEnding(int endingNo){
+        string ending;
         //endingText.text="Ending number "+ endingNo+ " triggered\n";
         // move to scene No. of ending
-        Debug.Log("Ending number "+ endingNo+ " triggered");
+        if(endingNo==1){
+          ending = "아니… 당연히 독사과죠…";
+        }
+        else if(endingNo==2){
+          ending = "공주는 승부욕이 강하다. 마스터 티어를 찍으려면 성을 떠날 수 없다.";
+        }
+        else if(endingNo==3){
+          ending = "백주부가 뿌린 설탕이 코에 들어갔다. 호흡곤란으로 그만….";
+        }
+        else if(endingNo==4){
+          ending = "앗, 욕쟁이 할머니었다. 공주는 마상을 입고 죽었다.";
+        }
+        else if(endingNo==5){
+          ending = "앗, 드론 파편이 공주의 미간에 꽂혔다.";
+        }
+        else if(endingNo==6){
+          ending = "앗, 미정.";
+        }
+        else if(endingNo==7){
+          ending = "공주는 어이가 없어서 죽었다.";
+        }
+        else if(endingNo==8){
+          ending = "앗, 미정.";
+        }
+        else if(endingNo==9){
+          ending = "앗, 미정.";
+        }
+        else if(endingNo==10){
+          ending = "앗, 미정.";
+        }
+        else if(endingNo==11){
+          ending = "앗, 미정.";
+        }
+        else if(endingNo==12){
+          ending = "잘 모르는 사람은 따라가면 안돼요…";
+        }
+        else if(endingNo==13){
+          ending = "공주는 진정한 천국이 무엇인지 알게 되었다. 탈출 안 해~";
+        }
+        else if(endingNo==14){
+          ending = "공주의 SOS 신호를 포착한 QN64-1행성의 특공대가 공주를 구출했다. 공주는 그렇게 태양계를 벗어났다.";
+        }
+        else if(endingNo==15){
+          ending = "앗, 미정.";
+        }
+        else if(endingNo==16){
+          ending = "엄지공주는 정말 작고 가벼워요!";
+        }
+        else if(endingNo==17){
+          ending = "제비가 엄지공주를 태우고 꽃의 나라로 향합니다.";
+        }
+        else if(endingNo==18){
+          ending = "거대해진 맥시무스가 용을 물리쳤다. 하지만 너무 커진 맥시무스는 공주와 왕자를 미처 발견하지 못하고…";
+        }
+        Debug.Log("Ending number" + endingNo + "triggered");
+
     }
 
     void Update(){
         if (dialogueActive){
             if (NPCActive){
                 if (currentNPC.hasOptions){
-                    optionsBox.SetActive(true);
                     int coefficient = currentNPC.options.Count;
-                    Vector3 selectorPos = optionSelector.transform.position;
                     if (Input.GetKeyDown(KeyCode.UpArrow)){
                         selectedOption = (selectedOption + coefficient - 1)%coefficient;
-                        selectorPos.y = optionsParent.transform.GetChild(selectedOption).transform.position.y;
                     }
                     if (Input.GetKeyDown(KeyCode.DownArrow)){
                         selectedOption = (selectedOption + 1)%coefficient;
-                        selectorPos.y = optionsParent.transform.GetChild(selectedOption).transform.position.y;
-                    } 
-                    
+                    }
+
                     if (Input.GetKeyDown(KeyCode.Return))
                     {
                         string temp = currentNPC.selectOption(selectedOption);
@@ -186,43 +232,29 @@ public class PlayerInteraction : MonoBehaviour
                         //TODO: Add Ending conditions here?
 
                         GameObject.Find("infoB").GetComponent<Text>().text = "";
-                        optionsBox.SetActive(false);
-                        selectorPos.y = optionsParent.transform.position.y;
+                        optionsParent.SetActive(false);
                     }
-                    optionSelector.transform.position = selectorPos;
-                } else{
-                    optionsBox.SetActive(false);
                 }
 
-               
+
             } else {
                 // Key bindings for dialogue UI
                 if (currentObj.hasOptions){
-                    optionsBox.SetActive(true);
-                    Vector3 selectorPos = optionSelector.transform.position;
                     int coefficient = currentObj.options.Length;
                     if (Input.GetKeyDown(KeyCode.UpArrow)){
                         selectedOption = (selectedOption + coefficient - 1)%coefficient;
-                        selectorPos.y = optionsParent.transform.GetChild(selectedOption).transform.position.y;
                     }
                     if (Input.GetKeyDown(KeyCode.DownArrow)){
                         selectedOption = (selectedOption + 1)%coefficient;
-                        selectorPos.y = optionsParent.transform.GetChild(selectedOption).transform.position.y;
-
                     }
-                    if (Input.GetKeyDown(KeyCode.Return))
-                    {
-                        GameObject.Find("infoA").GetComponent<Text>().text = currentObj.selectOption(selectedOption); // code in individual Objects
-                        GameObject.Find("infoB").GetComponent<Text>().text = "";
-                        selectorPos.y = optionsParent.transform.position.y;
-                        optionsBox.SetActive(false);
-                    }
-                    optionSelector.transform.position = selectorPos;
-                } else{
-                    optionsBox.SetActive(false);
                 }
 
-                
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    GameObject.Find("infoA").GetComponent<Text>().text = currentObj.selectOption(selectedOption); // code in individual Objects
+                    GameObject.Find("infoB").GetComponent<Text>().text = "";
+                    optionsParent.SetActive(false);
+                }
 
                 if (Input.GetKeyDown(KeyCode.Q)){
                     inventory.addItem(currentObj);
@@ -233,25 +265,20 @@ public class PlayerInteraction : MonoBehaviour
 
 
         if (doorActive){
-            Vector3 selectorPos = doorSelector.transform.position;
             int coefficient = currentDoor.goalPosition.Length;
 
             if (Input.GetKeyDown(KeyCode.UpArrow)){
                 selectedDoor = (selectedDoor + coefficient - 1)%coefficient;
-                selectorPos.y = doorParent.transform.GetChild(selectedDoor).transform.position.y;
 			}
 			if (Input.GetKeyDown(KeyCode.DownArrow)){
                 selectedDoor = (selectedDoor + 1)%coefficient;
-                selectorPos.y = doorParent.transform.GetChild(selectedDoor).transform.position.y;
             }
 
             if (Input.GetKeyDown(KeyCode.Return))
-			{   
+			{
                 teleported = true;
 				this.transform.position = currentDoor.getDestination(currentDoor.goalPosition[selectedDoor]); // This should be the coordinates for the moved location
-                selectorPos.y = doorParent.transform.position.y;
 			}
-           doorSelector.transform.position = selectorPos;
         }
     }
 }
