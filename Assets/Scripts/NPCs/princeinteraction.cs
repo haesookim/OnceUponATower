be169bbbac5d651140c1677924eb424adbc22760
sohhyung <lgb0324@naver.com>
2020-05-18 22:38:08 +0900

    door edited

--- Assets/Scripts/NPCs/princeinteraction.cs
+++ Assets/Scripts/NPCs/princeinteraction.cs
@@ -2,17 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class princeinteraction : MonoBehaviour
+public class princeinteraction : NPCInteraction
 {
+  public GameObject PlayerObject;
+  private PlayerInteraction Player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        NPCName="왕자";
+        infoA = "백마 탄 왕자다.";
+
+        hasOptions = true;
+        options = new List<string>{"말을 건다."};
+
+        Player = PlayerObject.GetComponent<PlayerInteraction>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public override string selectOption(int optionNo){
+        if (optionNo == 0){
+
+          if(Player.actionConditions[2]){
+            
+            Player.TriggerEnding(8);
+            return @"『공주님! 무사하셨군요. 아아… 정말 다행입니다.
+            용맹한 제가 용을 무찔렀어요. 이제 안심하셔도 됩니다.
+            일단 이곳을 빠져나가요. 맥시무스, 공주님을 도와드려.』";
+
+          }
+          else{
+            Player.TriggerEnding(6);
+            return @"『공주님! 무사하셨군요. 아아… 정말 다행입니다.
+            용맹한 제가 용을 무찔러 드릴게요. 이제 안심하셔도 됩니다.
+            맥시무스, 가자!』";
+
+          }
+
+        }
+        return null;
     }
+
+
+
+
 }
