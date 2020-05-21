1cf668330577bcb995ddaf08ae45adbba07917e2
Heather Kim <haesoo1108@gmail.com>
2020-05-22 00:42:24 +0900

    Fixed minor door interactions, fixed applejuice interaction

--- Assets/Scripts/NPCs/compressorInteraction.cs
+++ Assets/Scripts/NPCs/compressorInteraction.cs
@@ -26,12 +26,11 @@ public class compressorInteraction : NPCInteraction
     void Update()
     {
         if(Inventory.contains("사과") && !itemsAdded[0]){
-            addOption("사과를 집어넣는다","사과즙을 획득했다");
-            Debug.Log(actionText);
+            addOption("사과를 집어넣는다.","");
             itemsAdded[0]  = true;
         }
         if(Inventory.contains("송로버섯") && !itemsAdded[1]){
-            addOption("송로버섯을 집어넣는다", "트러플 오일을 획득했다");
+            addOption("송로버섯을 집어넣는다.", "");
             itemsAdded[1] = true;
         }
     }
@@ -40,14 +39,18 @@ public class compressorInteraction : NPCInteraction
         if(optionNo == 0){
             Player.TriggerEnding(10);
             return null;
-        } else if(optionNo == options.IndexOf("사과를 집어넣는다")){
+        } else if(optionNo == options.IndexOf("사과를 집어넣는다.")){
             Inventory.removeItem("사과");
             Inventory.replaceItem("사과즙", "사과를 갈아서 만든 즙이다.", applejuiceSprite);
-            return actionText[optionNo];
-        } else if (optionNo == options.IndexOf("송로버섯을 집어넣는다")){
+            options.Remove("사과를 집어넣는다.");
+            actionText.RemoveAt(optionNo);
+            return "사과즙을 획득했다";
+        } else if (optionNo == options.IndexOf("송로버섯을 집어넣는다.")){
             Inventory.removeItem("송로버섯");
             Inventory.replaceItem("트러플 오일", "고소한 향이 나는 트러플 오일이다.", truffleOilSprite);
-            return actionText[optionNo];
+            options.Remove("송로버섯을 집어넣는다.");
+            actionText.RemoveAt(optionNo);
+            return "트러플 오일을 획득했다.";
         }
         return null;
     }
