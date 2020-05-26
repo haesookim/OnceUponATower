dd7a4f03e4f96efcaba570e4013e5485580ece70
Heather Kim <haesoo1108@gmail.com>
2020-05-26 16:27:27 +0900

    Fix duplicate options bug

--- Assets/Scripts/NPCs/mrbaekinteraction.cs
+++ Assets/Scripts/NPCs/mrbaekinteraction.cs
@@ -5,38 +5,43 @@ using UnityEngine;
 public class mrbaekinteraction : NPCInteraction
 {
 
-  private bool truffleOil = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        NPCName = "백종원";
-        infoA = "백주부가 재료를 손질 중이다.";
+	private bool truffleOil = false;
+	// Start is called before the first frame update
+	void Start()
+	{
+		NPCName = "백종원";
+		infoA = "백주부가 재료를 손질 중이다.";
 
-        hasOptions = true;
-        options = new List<string>{"말을 건다."};
+		hasOptions = true;
+		options = new List<string> { "말을 건다." };
 
-        Inventory = PlayerObject.GetComponent<PlayerInventory>();
+		Inventory = PlayerObject.GetComponent<PlayerInventory>();
 
-        Player = PlayerObject.GetComponent<PlayerInteraction>();
-    }
+		Player = PlayerObject.GetComponent<PlayerInteraction>();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-      if (Inventory.contains("트러플 오일") && !truffleOil){
-        addOption("트러플 오일을 건넨다.", "");
-      }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if (Inventory.contains("트러플 오일") && !truffleOil)
+		{
+			addOption("트러플 오일을 건넨다.", "");
+			truffleOil = true;
+		}
+	}
 
-    public override string selectOption(int optionNo){
-      Player.optionsBox.SetActive(false);
-      if (optionNo == 0){
-        Player.TriggerEnding(3);
-      }
-      else if (optionNo ==1){
-        Player.TriggerEnding(13);
-        //animation
-      }
-      return null;
-    }
+	public override string selectOption(int optionNo)
+	{
+		Player.optionsBox.SetActive(false);
+		if (optionNo == 0)
+		{
+			Player.TriggerEnding(3);
+		}
+		else if (optionNo == 1)
+		{
+			Player.TriggerEnding(13);
+			//animation
+		}
+		return null;
+	}
 }
