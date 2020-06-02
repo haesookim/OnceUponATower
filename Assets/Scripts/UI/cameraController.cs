8f2d15643ec663702c592b7b6aecf23c226ed67d
Heather Kim <haesoo1108@gmail.com>
2020-06-02 17:31:21 +0900

    minor changes

--- Assets/Scripts/UI/cameraController.cs
+++ Assets/Scripts/UI/cameraController.cs
@@ -4,40 +4,48 @@ using UnityEngine;
 
 public class cameraController : MonoBehaviour
 {
-    public GameObject player;
-    public PlayerInteraction interaction;
-    public float offset;
-    private Vector3 velocity = Vector3.zero;
+	public GameObject player;
+	public PlayerInteraction interaction;
+	public float offset;
+	private Vector3 velocity = Vector3.zero;
 
-    void Start()
-    {
-        interaction = player.GetComponent<PlayerInteraction>();
-    }
+	void Start()
+	{
+		interaction = player.GetComponent<PlayerInteraction>();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {   
-        Vector3 newPos = this.transform.position;
-        
-        float positionOffset = this.transform.position.x - player.transform.position.x;
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 newPos = this.transform.position;
 
-        if (positionOffset>offset){
-            newPos.x = player.transform.position.x+offset;
-        } else if (positionOffset < -offset) {
-            newPos.x = player.transform.position.x-offset;
-        }
+		float positionOffset = this.transform.position.x - player.transform.position.x;
 
-        if (player.GetComponent<PlayerInteraction>().teleported){
-            //newPos.x = player.transform.position.x;
-            if (interaction.currentDoor.positionInfo == "left"){
-                newPos.x = player.transform.position.x - offset;
-            }
-            else if(interaction.currentDoor.positionInfo == "right"){
-                newPos.x = player.transform.position.x + offset;
-            }
-            newPos.y = player.transform.position.y + 1.6f;
-            player.GetComponent<PlayerInteraction>().teleported = false;
-        }
-        this.transform.position = newPos;
-    }
+		if (positionOffset > offset)
+		{
+			newPos.x = player.transform.position.x + offset;
+
+			//newpos =Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+		}
+		else if (positionOffset < -offset)
+		{
+			newPos.x = player.transform.position.x - offset;
+		}
+
+		if (player.GetComponent<PlayerInteraction>().teleported)
+		{
+			//newPos.x = player.transform.position.x;
+			if (interaction.currentDoor.positionInfo == "left")
+			{
+				newPos.x = player.transform.position.x - offset;
+			}
+			else if (interaction.currentDoor.positionInfo == "right")
+			{
+				newPos.x = player.transform.position.x + offset;
+			}
+			newPos.y = player.transform.position.y + 1.6f;
+			player.GetComponent<PlayerInteraction>().teleported = false;
+		}
+		this.transform.position = newPos;
+	}
 }
