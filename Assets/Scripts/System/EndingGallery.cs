85fb87ad4d9245ae6ae44028ed505f1004856757
Heather Kim <haesoo1108@gmail.com>
2020-06-03 23:09:08 +0900

    Finished building endinggalleryui

--- Assets/Scripts/System/EndingGallery.cs
+++ Assets/Scripts/System/EndingGallery.cs
@@ -11,23 +11,46 @@ public class EndingGallery : MonoBehaviour
 
 	public Text endingTitle;
 
+	public Image highlight;
+	int highlightXposOffset = 7;
+	int highlightYposOffset = 14;
+
+	int movingOffset = 150;
+
+	GameObject[] GalleryOptions = new GameObject[21];
+	public GameObject galleryParent;
+
+	public Camera Main;
+
 	// Start is called before the first frame update
 	void Start()
 	{
-
+		for (int i = 0; i < 21; i++)
+		{
+			GalleryOptions[i] = galleryParent.transform.GetChild(i).gameObject;
+		}
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.RightArrow) && endingNo < endingCount)
+		if (Input.GetKeyDown(KeyCode.RightArrow) && endingNo < endingCount - 1)
 		{
 			endingNo++;
+			galleryParent.transform.Translate(new Vector3(-movingOffset, 0, 0));
 		}
 		if (Input.GetKeyDown(KeyCode.LeftArrow) && endingNo > 0)
 		{
 			endingNo--;
+			galleryParent.transform.Translate(new Vector3(movingOffset, 0, 0));
 		}
+		Vector3 newpos = highlight.transform.position;
+		newpos.x = GalleryOptions[endingNo].transform.position.x + highlightXposOffset;
+		newpos.y = GalleryOptions[endingNo].transform.position.y + highlightYposOffset;
+		highlight.transform.position = newpos;
+
+
+
 		endingTitle.text = GameData.DataToSave.endingTitlesToSave[endingNo];
 		if (Input.GetKeyDown(KeyCode.I))
 		{
