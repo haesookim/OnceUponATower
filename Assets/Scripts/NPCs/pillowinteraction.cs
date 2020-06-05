974006b80c8563c4ab929bbfd0387c0b61367360
sohhyung <lgb0324@naver.com>
2020-06-06 06:58:52 +0900

    spinning wheel, pillow interaction added

---
+++ Assets/Scripts/NPCs/pillowinteraction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pillowinteraction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      NPCName = "베개";
+      infoA = "푹신한 베개다.";
+
+      hasOptions = true;
+      options = new List<string>{"베개를 베고 눕는다."};
+
+      actionText = new List<string>{"드론에 관한 논문을 읽었다. 금새 드론 척척박사가 되었다."};
+    }
+
+
+    public override string selectOption(int optionNo){
+        Player.optionsBox.SetActive(false);
+        if (optionNo == 0){
+          Player.TriggerEnding(20);
+        }
+        return null;
+    }
+
+
+    void Update(){
+        changeSprite();
+    }
+
+}
---
+++ Assets/Scripts/NPCs/spinningwheelinteraction.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spinningwheelinteraction : MonoBehaviour
+{
+    public bool itemAdded = false;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+      NPCName = "물레";
+      infoA = "표면이 거친 물레다.";
+
+      hasOptions = true;
+      options = new List<string>{"살펴본다."};
+      actionText = new List<string>{"아얏"};
+
+      Player = PlayerObject.GetComponent<PlayerInteraction>();
+      Inventory = PlayerObject.GetComponent<PlayerInventory>();
+    }
+
+
+    public override string selectOption(int optionNo){
+        Player.optionsBox.SetActive(false);
+        if (optionNo == 0){
+          Player.TriggerEnding(21);
+          return null;
+        }
+        if(optionNo == 1){
+          Inventory.removeItem("누에고치");
+          Inventory.replaceItem("명주실");
+          options.Remove("고치로 실을 뽑는다.");
+          return actionText[optionNo];
+
+        }
+        return null;
+    }
+
+
+    void Update(){
+        changeSprite();
+        if(Inventory.contains("누에고치")&& !itemAdded){
+          addOption("고치로 실을 뽑는다.", "윤기가 자르르한 명주실이 만들어졌다. 마치… 라푼젤의 머리 같다.");
+          itemAdded = true;
+        }
+
+    }
+}
