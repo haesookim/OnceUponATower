ca5d4bfcf15ce1fafdbc0b4803537a4ac8e7e049
Heather Kim <haesoo1108@gmail.com>
2020-06-06 11:54:46 +0900

    Added pillow and spinning wheel npcs

--- Assets/Scripts/NPCs/pillowinteraction.cs
+++ Assets/Scripts/NPCs/pillowinteraction.cs
@@ -2,32 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pillowinteraction : MonoBehaviour
+public class pillowinteraction : NPCInteraction
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-      NPCName = "베개";
-      infoA = "푹신한 베개다.";
+	// Start is called before the first frame update
+	void Start()
+	{
+		NPCName = "베개";
+		infoA = "푹신한 베개다.";
 
-      hasOptions = true;
-      options = new List<string>{"베개를 베고 눕는다."};
+		hasOptions = true;
+		options = new List<string> { "베개를 베고 눕는다." };
 
-      actionText = new List<string>{"드론에 관한 논문을 읽었다. 금새 드론 척척박사가 되었다."};
-    }
-
-
-    public override string selectOption(int optionNo){
-        Player.optionsBox.SetActive(false);
-        if (optionNo == 0){
-          Player.TriggerEnding(20);
-        }
-        return null;
-    }
+		actionText = new List<string> { "드론에 관한 논문을 읽었다. 금새 드론 척척박사가 되었다." };
+	}
 
 
-    void Update(){
-        changeSprite();
-    }
+	public override string selectOption(int optionNo)
+	{
+		Player.optionsBox.SetActive(false);
+		if (optionNo == 0)
+		{
+			Player.TriggerEnding(20);
+		}
+		return null;
+	}
+
+
+	void Update()
+	{
+		changeSprite();
+	}
 
 }
