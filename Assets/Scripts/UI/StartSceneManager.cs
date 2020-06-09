661cde34d48f8be1cf1935233f6f6c519cfe1f45
Heather Kim <haesoo1108@gmail.com>
2020-06-09 14:48:39 +0900

    Fixed Startscene text issue

---
+++ Assets/Scripts/UI/StartSceneManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class StartSceneManager : MonoBehaviour
+{
+	public void loadGallery()
+	{
+		GameData.DataToSave.LoadGame();
+		SceneManager.LoadScene("GalleryScene");
+	}
+
+	public void loadPlay()
+	{
+		GameData.DataToSave.LoadGame();
+		SceneManager.LoadScene("StartText");
+	}
+}
