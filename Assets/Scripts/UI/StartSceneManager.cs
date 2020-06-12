297a6d12b5152d12cfbbf0fbab25baf6f6e31e78
Heather Kim <haesoo1108@gmail.com>
2020-06-12 20:52:07 +0900

    :construction: Adding new endingscene, edited startscene for keyboard input

--- Assets/Scripts/UI/StartSceneManager.cs
+++ Assets/Scripts/UI/StartSceneManager.cs
@@ -6,6 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class StartSceneManager : MonoBehaviour
 {
+	public GameObject startButton;
+	public GameObject galleryButton;
+	public GameObject highlight;
+
+	private int selectedOption = 0;
 	public void loadGallery()
 	{
 		GameData.DataToSave.LoadGame();
@@ -16,5 +21,40 @@ public class StartSceneManager : MonoBehaviour
 	{
 		GameData.DataToSave.LoadGame();
 		SceneManager.LoadScene("StartText");
+	}
+
+	void Update()
+	{
+		Vector3 selectorPos = highlight.transform.position;
+		int coefficient = 2;
+
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			selectedOption = (selectedOption + coefficient - 1) % coefficient;
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			selectedOption = (selectedOption + 1) % coefficient;
+		}
+
+		if (selectedOption == 0)
+		{
+			selectorPos.y = startButton.transform.position.y - 0.1f;
+		}
+		else
+		{ selectorPos.y = galleryButton.transform.position.y - 0.1f; }
+
+		if (Input.GetKeyDown(KeyCode.LeftControl))
+		{
+			if (selectedOption == 0)
+			{
+				loadPlay();
+			}
+			else
+			{
+				loadGallery();
+			}
+		}
+		highlight.transform.position = selectorPos;
 	}
 }
